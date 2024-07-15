1ee14b1b977bd633babcb769818fa38a9106ebc8
Dimitar <91668931+mitky@users.noreply.github.com>
2024-07-15 11:41:40 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# Ensure DEPLOYMENT_ID is set properly in your deployment environment
+DEPLOYMENT_ID="d-JMCIQ2P25"
+
 # Get the deployment directory path using DEPLOYMENT_ID
 DEPLOYMENT_ROOT="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_ID/deployment-archive"
 
 echo "Deployment directory: $DEPLOYMENT_ROOT"
 
+# Check if the deployment directory exists
+if [ ! -d "$DEPLOYMENT_ROOT" ]; then
+  echo "Error: Deployment directory $DEPLOYMENT_ROOT not found. Exiting..."
+  exit 1
+fi
+
 # Navigate to the deployment directory
-cd $DEPLOYMENT_ROOT
+cd $DEPLOYMENT_ROOT || exit
 
 # Install npm packages
 echo "Installing npm packages..."
-npm install --production  # Use --production flag if you don't need development dependencies
+npm install --production --omit=dev
 
 # Check if npm packages are installed correctly
 if [ $? -ne 0 ]; then
@@ -18,10 +27,10 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+echo "Dependencies installed successfully."
+
 # Additional commands if needed, like building or compiling assets
 # Example: npm run build
-
-echo "Dependencies installed successfully."
 
 # Ensure proper file permissions if necessary
 # Example: chmod +x some_script.sh
