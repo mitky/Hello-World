90d5946958772842165a6a301f0a7e46ea2ae970
mitky <dimitar.shikrenov@gmail.com>
2024-08-08 12:27:21 +0100

    trigger_builds.sh

---
+++ scripts/trigger_builds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Number of parallel builds
+BUILD_COUNT=10
+
+# Path to your Cloud Build configuration file
+CONFIG_FILE="cloudbuild.yaml"
+
+# Trigger builds in parallel
+for i in $(seq 1 $BUILD_COUNT); do
+    echo "Triggering build $i"
+    gcloud builds submit --config=$CONFIG_FILE &
+done
+
+# Wait for all background processes to complete
+wait
+
+echo "All builds triggered."
